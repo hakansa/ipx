b7f2effc67d3a70e4b083b1f0fee5286150eb60c
Hakan SARIMAN <hknsrmn46@gmail.com>
2021-05-12 21:03:21 +0300

    parseCIDR func added

--- ipnet.go
+++ ipnet.go
@@ -11,6 +11,23 @@ type IPNet struct {
 	Mask IPMask // network mask
 }
 
+// ParseCIDR parses s as a CIDR notation IP address and prefix length,
+// like "192.0.2.0/24" or "2001:db8::/32", as defined in
+// RFC 4632 and RFC 4291.
+//
+// It returns the IP address and the network implied by the IP and
+// prefix length.
+// For example, ParseCIDR("192.0.2.1/24") returns the IP address
+// 192.0.2.1 and the network 192.0.2.0/24.
+func ParseCIDR(s string) (IP, *IPNet, error) {
+	ip, ipNet, err := net.ParseCIDR(s)
+	if err != nil {
+		return IP{}, nil, err
+	}
+
+	return IP{ip}, &IPNet{IP{ipNet.IP}, IPMask{ipNet.Mask}}, nil
+}
+
 // Contains reports whether the network includes ip.
 func (n *IPNet) Contains(ip IP) bool {
 	nn, m := networkNumberAndMask(n)
